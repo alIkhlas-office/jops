b996ed96429190705288edc3e81016e21f833b5c
s2yed <s2yedftouh@gmail.com>
2026-05-21 21:27:58 +0300

    in

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ضع هنا رابط تطبيق Google Apps Script بعد نشره (Deploy > Web app).
 // مثال: https://script.google.com/macros/s/AKfyc.../exec
-window.SHEET_ENDPOINT = "ضع_رابط_APPS_SCRIPT_هنا";
+window.SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbxoRHp_j7lUZgrsIYKs6n714yFmEYL8xIXbk2-_ruf8aB54Km-2qpso_IVZTkRVS5ex/exec";
